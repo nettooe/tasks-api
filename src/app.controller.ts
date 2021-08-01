659f171f0203458bf8604ada5bfb394fd18b1bc3
nettooe <netto.oe@gmail.com>
2021-07-31 22:21:21 -0300

    Primeiro crud em memória

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getTask(): string {
+    return this.appService.getHello();
+  }
+
+//  @Get(':id')
+//  getTaskById(@Param('id') id: string): string {
+//    return this.appService.getHello() + id;
+//  }
+
+
+}
